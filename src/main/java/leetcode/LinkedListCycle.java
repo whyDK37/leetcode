9e3dc5243f81f533f688e522dce89edd6cd274eb
Wang Huanyu <wanghuanyu@imdada.cn>
2018-11-16 20:49:37 +0800

    LinkedListCycle

---
+++ src/main/java/leetcode/LinkedListCycle.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class LinkedListCycle {
+
+    //     Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        if (head.next == null) return false;
+
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null &&
+                slow != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ src/test/java/leetcode/LinkedListCycleTest.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import leetcode.LinkedListCycle.ListNode;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class LinkedListCycleTest {
+
+    @Test
+    public void testHasCycle() {
+        ListNode head = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+
+        head.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = five;
+        five.next = head;
+
+        assertTrue(LinkedListCycle.hasCycle(head));
+        five.next = two;
+        assertTrue(LinkedListCycle.hasCycle(head));
+        five.next = three;
+        assertTrue(LinkedListCycle.hasCycle(head));
+        five.next = four;
+        assertTrue(LinkedListCycle.hasCycle(head));
+        five.next = five;
+        assertTrue(LinkedListCycle.hasCycle(head));
+
+        five.next = null;
+        assertFalse(LinkedListCycle.hasCycle(head));
+    }
+}
