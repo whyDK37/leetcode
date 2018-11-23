cb24149f789c6c0255449a9dbf18560cc421abb8
Wang Huanyu <wanghuanyu@imdada.cn>
2018-11-23 20:56:32 +0800

    IntersectionOfTwoLinkedLists.java

---
+++ src/main/java/leetcode/IntersectionOfTwoLinkedLists2.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+
+/**
+ */
+public class IntersectionOfTwoLinkedLists2 {
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = length(headA), lenB = length(headB);
+        // move headA and headB to the same start point
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+        // find the intersection until end
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private static int length(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+}
