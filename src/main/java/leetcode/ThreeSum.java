f04645b02b87a2dd6fdf48c78934a6e8ce5400d6
Wang Huanyu <wanghuanyu@imdada.cn>
2018-12-20 20:39:15 +0800

    ThreeSum.java

--- src/main/java/leetcode/ThreeSum.java
+++ src/main/java/leetcode/ThreeSum.java
@@ -7,14 +7,18 @@ public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         if (nums.length < 3)
             return new ArrayList<>();
+        // sort
         Arrays.sort(nums);
         List<List<Integer>> resultList = new ArrayList<>();
         for (int i = 0; i < nums.length - 1; i++) {
 
+            // pick one element which not used before
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
+            // match sum
             int complement = -nums[i];
+            // two point, point to left and right,
             int left = i + 1;
             int right = nums.length - 1;
             while (left < right) {
@@ -26,8 +30,7 @@ public class ThreeSum {
                     tempList.add(nums[right]);
                     resultList.add(tempList);
 
-
-                    // skip same element
+                    // skip same left/right element, avoid duplicate result
                     left++;
                     right--;
                     while (left < right && left > 0 && nums[left] == nums[left - 1]) {
