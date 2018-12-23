4adf8bf8e287023240f084ee09fdf0d1657e2bfc
Wang Huanyu <wanghuanyu@imdada.cn>
2018-12-23 12:56:31 +0800

    add test

--- src/test/java/leetcode/ValidParenthesesTest.java
+++ src/test/java/leetcode/ValidParenthesesTest.java
@@ -8,9 +8,11 @@ public class ValidParenthesesTest {
 
     @Test
     public void testIsValid() {
+        assertTrue(new ValidParentheses().isValid("((()(())))"));
         assertTrue(new ValidParentheses().isValid("()"));
         assertTrue(new ValidParentheses().isValid("()[]{}"));
         assertTrue(new ValidParentheses().isValid("{[]}"));
+        assertFalse(new ValidParentheses().isValid("(((((((()"));
         assertFalse(new ValidParentheses().isValid("(]"));
         assertFalse(new ValidParentheses().isValid("([)]"));
     }
