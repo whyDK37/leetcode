5d367862b01bb3b13220c1d874e7afba180ff4b6
Wang Huanyu <wanghuanyu@imdada.cn>
2018-12-29 21:02:51 +0800

    add doc

---
+++ src/test/java/leetcode/MergeKSortedLists2Test.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.testng.annotations.Test;
+
+import static leetcode.ListNodeUtil.createListNode;
+import static leetcode.ListNodeUtil.equalsListNode;
+import static org.testng.Assert.*;
+
+public class MergeKSortedLists2Test {
+
+    @Test
+    public void testMergeKLists() {
+        ListNode listNode = new MergeKSortedLists2().mergeKLists(new ListNode[]{createListNode(1, 4, 5),
+                createListNode(1, 3, 4),
+                createListNode(2, 6)});
+        equalsListNode(listNode, new int[]{1, 1, 2, 3, 4, 4, 5, 6});
+    }
+}
