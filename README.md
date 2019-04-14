86f025463f2f0ddca1fb721581741ea72fa4f29d
Wang Huanyu <wanghuanyu@imdada.cn>
2019-04-14 15:51:57 +0800

    JumpGame.java

--- README.md
+++ README.md
@@ -33,6 +33,7 @@ leetcode
 |48|[rotate-image](https://leetcode.com/problems/rotate-image/)|  [Go](golang/main/RotateImage.go)|Medium|
 |49|[group-anagrams](https://leetcode.com/problems/group-anagrams/)|  [java](./src/main/java/leetcode/GroupAnagrams.java)|Medium|
 |53|[maximum-subarray](https://leetcode.com/problems/maximum-subarray/)|  [java](./src/main/java/leetcode/MaximumSubarray.java)|Easy|
+|55|[jump-game](https://leetcode.com/problems/jump-game/)|  [java](./src/main/java/leetcode/JumpGame.java)|Medium|
 |58|[length-of-last-word](https://leetcode.com/problems/length-of-last-word/)| [java](./src/main/java/leetcode/LengthOfLastWord.java)|Easy|
 |92|[reverse-linked-list-ii](https://leetcode.com/problems/reverse-linked-list-ii/)| [java](./src/main/java/leetcode/ReverseLinkedList2.java)|Medium|
 |141|[linked-list-cycle](https://leetcode.com/problems/linked-list-cycle/)| [java](./src/main/java/leetcode/LinkedListCycle.java)|Easy|
---
+++ src/main/java/leetcode/JumpGame.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+
+public class JumpGame {
+    public boolean canJumpFromPosition(int position, int[] nums) {
+        if (position == nums.length - 1) {
+            return true;
+        }
+
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+//         Old
+//        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++)
+// New
+        for (int nextPosition = furthestJump; nextPosition > position; nextPosition--) {
+            if (canJumpFromPosition(nextPosition, nums)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+//        return canJumpFromPosition(0, nums);
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
