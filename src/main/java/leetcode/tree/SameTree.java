23681ab713d1d44c254a962c3ed665695f6c3347
liucaifucom@163.com <liucaifucom@163.com>
2019-09-14 08:41:38 +0800

    SameTree.java

---
+++ src/main/java/leetcode/tree/SameTree.java
@@ -0,0 +1,19 @@
+package leetcode.tree;
+
+import leetcode.pojo.TreeNode;
+
+/**
+ * DFS
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return isSameNode(p, q);
+    }
+
+    private boolean isSameNode(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        return p.val == q.val && isSameNode(p.left, q.left) && isSameNode(p.left, q.right);
+    }
+}
---
+++ src/test/java/leetcode/tree/SameTreeTest.java
@@ -0,0 +1,13 @@
+package leetcode.tree;
+
+import org.testng.Assert;
+import util.TestUtil;
+
+
+public class SameTreeTest {
+    public static void main(String[] args) {
+        SameTree sameTree = new SameTree();
+        Assert.assertTrue(sameTree.isSameTree(TestUtil.buildTree(new Integer[]{1, 2, 3}),
+                TestUtil.buildTree(new Integer[]{1, 2, 3})));
+    }
+}
