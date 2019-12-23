b47ac3407ec98029b4b13b84d51a9ad517367fb9
djwanghuanyu <CFSGuN#7>
2019-12-23 13:39:35 +0800

    add code

---
+++ src/test/java/leetcode/ImplementStrstrTest.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ImplementStrstrTest {
+
+    @Test
+    public void bf() {
+        ImplementStrstr str = new ImplementStrstr();
+        Assert.assertEquals(str.BF("aaaaaaaaaaaaaaaa", "baaa"), -1);
+        Assert.assertEquals(str.BF("aaaaab", "ab"), 4);
+        Assert.assertEquals(str.BF("hello", ""), 0);
+        Assert.assertEquals(str.BF("hello", "ll"), 2);
+        Assert.assertEquals(str.BF("aaaaa", "bba"), -1);
+    }
+}
