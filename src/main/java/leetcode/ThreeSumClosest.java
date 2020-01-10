1db6506c7099c73a69f8663beaa69c203fe3e054
djwanghuanyu <CFSGuN#7>
2020-01-10 19:35:45 +0800

    add code

--- src/main/java/leetcode/ThreeSumClosest.java
+++ src/main/java/leetcode/ThreeSumClosest.java
@@ -1,8 +1,28 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
 
-        return 0;
+        int ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - sum) < Math.abs(target - ans)) {
+                    ans = sum;
+                }
+                if (sum < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+
+        }
+        return ans;
     }
 }
