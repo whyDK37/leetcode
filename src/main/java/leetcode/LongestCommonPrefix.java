a4d5fe826031132e1839d6fce2469c1f0bbfb8de
djwanghuanyu <CFSGuN#7>
2020-01-14 20:22:13 +0800

    add code

---
+++ src/main/java/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import org.testng.Assert;
+
+/**
+ * @author why
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String pre = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(pre)) {
+                pre = pre.substring(0, pre.length() - 1);
+                if ("".equals(pre)) return pre;
+            }
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix prefix = new LongestCommonPrefix();
+        Assert.assertEquals(prefix.longestCommonPrefix(new String[]{"hello", "hello"}), "hello");
+        Assert.assertEquals(prefix.longestCommonPrefix(new String[]{"flower", "flow", "flight"}), "fl");
+        Assert.assertEquals(prefix.longestCommonPrefix(new String[]{"dog", "racecar", "car"}), "");
+    }
+}
