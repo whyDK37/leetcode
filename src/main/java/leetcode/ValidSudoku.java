6da1232b01a25aac3e92267c921c1f9524114be2
djwanghuanyu <CFSGuN#7>
2020-01-14 20:37:39 +0800

    add code

--- src/main/java/leetcode/ValidSudoku.java
+++ src/main/java/leetcode/ValidSudoku.java
@@ -2,13 +2,38 @@ package leetcode;
 
 import org.testng.Assert;
 
+import java.util.HashMap;
+
 /**
  * @author why
  */
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
+        HashMap<Character, Integer>[] rows = new HashMap[9];
+        HashMap<Character, Integer>[] columns = new HashMap[9];
+        HashMap<Character, Integer>[] boxes = new HashMap[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashMap<>();
+            columns[i] = new HashMap<>();
+            boxes[i] = new HashMap<>();
+        }
 
-        return false;
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] == '.') continue;
+                if (rows[row].putIfAbsent(board[row][col], 1) != null) {
+                    return false;
+                }
+                if (columns[col].putIfAbsent(board[row][col], 1) != null) {
+                    return false;
+                }
+                int box_index = (row / 3) * 3 + col / 3;
+                if (boxes[box_index].putIfAbsent(board[row][col], 1) != null) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
