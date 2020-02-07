9c87367fbcfd258654eb2de5bc2bf028b49e9bb6
why <Wang Huanyu>
2020-02-07 22:09:51 +0800

    code

--- src/main/java/leetcode/ValidateBinarySearchTree.java
+++ src/main/java/leetcode/ValidateBinarySearchTree.java
@@ -12,12 +12,8 @@ public class ValidateBinarySearchTree {
     private boolean isValid(TreeNode root, Integer min, Integer max) {
         if (root == null) return true;
 
-        if (root.left != null && root.left.val >= root.val) return false;
-        if (root.left != null && min != null && (root.left.val <= min))
-            return false;
-        if (root.right != null && root.right.val <= root.val) return false;
-        if (root.right != null && max != null && (root.right.val >= max))
-            return false;
+        if (min != null && root.val <= min) return false;
+        if (max != null && root.val >= max) return false;
 
         return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
     }
