272c1d1e846f033568021e7462005871640a9bd6
why <wanghuanyu@imdada.cn>
2020-03-02 19:45:12 +0800

    add code

---
+++ src/main/java/leetcode/tree/LinkedListInBinaryTree.java
@@ -0,0 +1,12 @@
+package leetcode.tree;
+
+import leetcode.ListNode;
+import leetcode.pojo.TreeNode;
+
+public class LinkedListInBinaryTree {
+
+  public boolean isSubPath(ListNode head, TreeNode root) {
+
+    return true;
+  }
+}
