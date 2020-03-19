36cb639b098089201cceb136860bea809b5a0fb2
why <wanghuanyu@imdada.cn>
2020-03-19 17:37:58 +0800

    add code

--- src/test/java/leetcode/EditDistanceTest.java
+++ src/test/java/leetcode/EditDistanceTest.java
@@ -9,6 +9,8 @@ public class EditDistanceTest {
   @Test
   public void testMinDistance() {
     EditDistance editDistance = new EditDistance();
+    assertEquals(editDistance.minDistance("成功，", "没看完"), 3);
+    assertEquals(editDistance.minDistance("h", "h"), 0);
     assertEquals(editDistance.minDistance("horse", "ros"), 3);
     assertEquals(editDistance.minDistance("intention", "execution"), 5);
   }
