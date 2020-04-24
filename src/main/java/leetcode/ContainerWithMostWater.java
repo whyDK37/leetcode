21dc524c01be8913f85d0efbf840de5bf3cd5919
why <why.DK37@gmail.com>
2020-04-24 20:08:54 +0800

    code

--- src/main/java/leetcode/ContainerWithMostWater.java
+++ src/main/java/leetcode/ContainerWithMostWater.java
@@ -8,15 +8,10 @@ public class ContainerWithMostWater {
       return 0;
     }
 
-    int start = 0, end = height.length - 1, maxarea = 0;
-    while (end > start) {
-      if (height[start] < height[end]) {
-        maxarea = Math.max(maxarea, height[start] * (end - start));
-        start++;
-      } else {
-        maxarea = Math.max(maxarea, height[end] * (end - start));
-        end--;
-      }
+    int maxarea = 0;
+    for (int start = 0, end = height.length - 1; start < end; ) {
+      maxarea = Math.max(maxarea,
+          (end - start) * (height[start] > height[end] ? height[end--] : height[start++]));
     }
     return maxarea;
   }
