e2089d7e97f977827c377575f8cdf08cbd48d4d6
why <why.DK37@gmail.com>
2020-04-24 17:00:26 +0800

    code

--- src/main/java/leetcode/MoveZeroes.java
+++ src/main/java/leetcode/MoveZeroes.java
@@ -7,16 +7,14 @@ public class MoveZeroes {
       return;
     }
 
-    int start = 0;
+    int index = 0;
     for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != 0 && start == i) {
-        start++;
-        continue;
-      }
       if (nums[i] != 0) {
-        int num = nums[i];
-        nums[start++] = num;
-        nums[i] = 0;
+        nums[index] = nums[i];
+        if (i != index) {
+          nums[i] = 0;
+        }
+        index++;
       }
 
     }
