fbb28b0c6559a051352e5110dcc25e05425f0fcf
why <why.DK37@gmail.com>
2020-05-05 22:14:31 +0800

    code

--- src/main/java/leetcode/LargestRectangleInHistogram.java
+++ src/main/java/leetcode/LargestRectangleInHistogram.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
 
   public int largestRectangleArea(int[] heights) {
@@ -8,14 +10,20 @@ public class LargestRectangleInHistogram {
       return 0;
     }
 
-    int area = 0;
+    Stack<Integer> stack = new Stack<>();
+    stack.push(-1);
+
+    int maxArea = 0;
     for (int i = 0; i < heights.length; i++) {
-      int min = Integer.MAX_VALUE;
-      for (int j = i; j < heights.length; j++) {
-        min = Math.min(min, heights[j]);
-        area = Math.max((j - i + 1) * min, area);
+      while (stack.peek() != -1 && heights[stack.peek()] > heights[i]) {
+        maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
       }
+      stack.push(i);
     }
-    return area;
+
+    while (stack.peek() != -1) {
+      maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+    }
+    return maxArea;
   }
 }
--- src/test/java/leetcode/LargestRectangleInHistogramTest.java
+++ src/test/java/leetcode/LargestRectangleInHistogramTest.java
@@ -9,6 +9,7 @@ public class LargestRectangleInHistogramTest {
   @Test
   public void testLargestRectangleArea() {
     LargestRectangleInHistogram rectangle = new LargestRectangleInHistogram();
+    assertEquals(rectangle.largestRectangleArea(new int[]{2, 1, 2}), 3);
     assertEquals(rectangle.largestRectangleArea(new int[]{0, 9}), 9);
     assertEquals(rectangle.largestRectangleArea(new int[]{}), 0);
     assertEquals(rectangle.largestRectangleArea(new int[]{2}), 2);
